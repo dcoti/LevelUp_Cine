cd653af1b75eb01bb12cbf5d840540eb1d75380f
Daniel Coti <danielcoti7@gmail.com>
2022-11-18 17:10:53 -0600

    movies

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, url_for, redirect,flash
+from flask import Flask, render_template, request, url_for, redirect,flash,session
+from sqlalchemy.sql import select
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\Daniel Coti\\Docume
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 db = SQLAlchemy(app)
+user_=None
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -19,13 +20,11 @@ class User(db.Model):
     phone = db.Column(db.Integer)
     created_at = db.Column(db.DateTime(), default=datetime.now())
 
-"""
-
 class Movie(db.Model):
     __tablename__ = 'movies'
     id_movie= db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(256))
-    img= db.Column(db.String(max))
+    img= db.Column(db.String(256))
     classification = db.Column(db.String(10))
     created_at = db.Column(db.DateTime(), default=datetime.now())
     
@@ -34,15 +33,15 @@ class Function(db.Model):
     id_function= db.Column(db.Integer, primary_key=True, autoincrement=True)
     seat = db.Column(db.String(256))
     datefuncion = db.Column(db.DateTime())
-    #id_movie = db.Column(db.Integer, db.ForeignKey('movies.id_movie'))
+    id_movie = db.Column(db.Integer, db.ForeignKey('movies.id_movie'))
     created_at = db.Column(db.DateTime(), default=datetime.now()) 
 
 class Ticket(db.Model):
     __tablename__ = 'tickets'
     id_ticket= db.Column(db.Integer, primary_key = True)
     seat= db.Column(db.String(256))
-    #id_function = db.Column(db.Integer, db.ForeignKey('functions.id_function'))  
-    created_at = db.Column(db.DateTime(), default=datetime.now()) """
+    id_function = db.Column(db.Integer, db.ForeignKey('functions.id_function'))  
+    created_at = db.Column(db.DateTime(), default=datetime.now()) 
 
 @app.route('/')
 def index():
@@ -51,9 +50,32 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        pass
+        user= db.session.query(User).filter(User.email==request.form['username']).first()
+        if user==None:
+            flash("Username does not exist")
+            return render_template('auth/login.html')
+        else:
+            if user.password==request.form['password']:
+                session["user_session"]=request.form['username']
+                global user_
+                user_=request.form['username']
+                return redirect('home')
+                #return render_template('/inicio.html',us=us,em=em,movies=movies)
+            else:
+                flash("Password incorrect")
+                return render_template('auth/login.html')  
     else:
         return render_template('auth/login.html')
+
+@app.route('/home', methods=['GET'])
+def home():
+    user= db.session.query(User).filter(User.email==user_).first()
+    us=user.username;
+    em=user.email;
+    movies=Movie.query.all()
+    return render_template('/inicio.html',us=us,em=em,movies=movies)
+
+
 
 @app.route('/sing_up', methods=['GET','POST'])
 def sing_up():
@@ -69,6 +91,29 @@ def sing_up():
         return render_template('auth/sing_up.html')
     else:
         return render_template('auth/sing_up.html')
+
+@app.route('/upload_movie', methods=['GET','POST'])
+def upload_upload():
+    if request.method== 'POST':
+        movie= Movie(title=request.form['title'],img=request.form['img'],classification=request.form['classification'])
+        mov=db.session.query(Movie).filter(Movie.title==movie.title).first()
+        if mov ==None:
+            db.session.add(movie)
+            db.session.commit()
+            flash("movie added")
+        else:
+            flash("movie already exists")
+        return render_template('auth/upload_movie.html')      
+    else:
+        return render_template('auth/upload_movie.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user_session', None)
+    global user_
+    user_= None
+    return redirect('/login')
+    
 
 
 if __name__ == '__main__':
